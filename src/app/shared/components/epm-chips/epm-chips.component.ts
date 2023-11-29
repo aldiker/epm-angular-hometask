498bfcc50cac83b1a9a9184fa632f4bb97d8dbe3
aldiker <aldiker@ukr.net>
2023-11-29 16:53:47 +0200

    Added chip component

---
+++ src/app/shared/components/epm-chips/epm-chips.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { INTEREST_CHIPS_NAMES } from '../../../app.config';
+
+@Component({
+  selector: 'epm-chips',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './epm-chips.component.html',
+  styleUrls: ['./epm-chips.component.scss']
+})
+export class EpmChipsComponent {
+  @Input() chips!: string;
+  @Input() isEditable = false;
+  @Input() isActive = false;
+
+  @Output() editChipsStatus: EventEmitter<string> = new EventEmitter<string>();
+
+  onClick(): void {
+    if (this.isEditable) {
+      this.editChipsStatus.emit(this.chips);
+    }
+  }
+
+  getChipIconSrc(chip: string): string {
+    return `assets/icons/${chip.toLowerCase()}.svg`;
+  }
+}
